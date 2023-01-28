393a5f83b98262f5699e8a27125c0baa827d4bcb
Erikas Arcikauskas <75033114+erikonasz@users.noreply.github.com>
2023-01-28 11:55:59 +0200

    Changed connection to use a global one

--- main.py
+++ main.py
@@ -15,36 +15,6 @@ connection = mysql.connector.connect(
 )   
 cursor = connection.cursor()
 
-def log_request_to_db(path, method, status):
-    connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='carslist',
-        port=4306
-    )
-    cursor = connection.cursor()
-
-    query = "INSERT INTO requests (path, method, status, time) VALUES (%s, %s, %s, %s)"
-    time = datetime.datetime.now()
-    cursor.execute(query, (path, method, status, time))
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-def log_request_decorator(endpoint_func):
-    async def wrapper(*args, **kwargs):
-        request = kwargs['request']
-        log_request_to_db(request.url.path, request.method, 200)
-        return await endpoint_func(*args, **kwargs)
-    return wrapper
-
-def insert_data(title, price, liter, fuel_type, year, transmission, city):
-    query = "INSERT INTO carslist (title, price, liter, fuel_type, year, transmission, city) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    cursor.execute(query, (title, price, liter, fuel_type, year, transmission, city))
-    connection.commit()
-    print("Data inserted successfully.")
-
 @app.get("/{title}")
 def get_record(title: str):
     query = f"SELECT * FROM carslist WHERE title = '{title}'"
@@ -52,7 +22,6 @@ def get_record(title: str):
     record = cursor.fetchone()
     return {"title": title, "data": record}
 
-@log_request_decorator
 @app.get("/cars/{count}")
 def get_cars_endpoint(count: int, request: Request):
     url = "https://autogidas.lt/en/skelbimai/automobiliai/?f_1%5B0%5D=BMW&f_model_14%5B0%5D=Serija+3&f_215=&f_216=&f_41=&f_42=&f_376="
@@ -66,7 +35,20 @@ def get_cars_endpoint(count: int, request: Request):
         transmission = car['transmission']
         city = car['city']
         insert_data(title, price, liter, fuel_type, year, transmission, city)
+    log_request_to_db(request.url.path, request.method, 200)
     return {"cars": cars}
+
+def log_request_to_db(path, method, status):
+    query = "INSERT INTO requests (path, method, status, time) VALUES (%s, %s, %s, %s)"
+    time = datetime.datetime.now()
+    cursor.execute(query, (path, method, status, time))
+    connection.commit()
+
+def insert_data(title, price, liter, fuel_type, year, transmission, city):
+    query = "INSERT INTO carslist (title, price, liter, fuel_type, year, transmission, city) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(query, (title, price, liter, fuel_type, year, transmission, city))
+    connection.commit()
+    print("Data inserted successfully.")
 
 @app.get("/health")
 def health_check():
@@ -75,19 +57,8 @@ def health_check():
 # Get 10 most recent requests
 @app.get("/recent_requests")
 async def recent_requests():
-    connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='carslist',
-        port=4306
-    )
-    cursor = connection.cursor()
-
     query = "SELECT endpoint_name, local_datetime FROM requests ORDER BY local_datetime DESC LIMIT 10"
     cursor.execute(query)
     records = cursor.fetchall()
     connection.commit()
-    cursor.close()
-    connection.close()
     return {"requests": records}
